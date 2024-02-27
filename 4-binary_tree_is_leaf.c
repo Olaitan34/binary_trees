2aa037497bfddf3e1bafbc2b456b54e7c0336d75
emmfatsneh@gmail.com <emmfatsneh@gmail.com>
2024-02-27 20:00:50 +0100

    la

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - To check if the node is a leaf
+ * @tree: tree to check
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->right == NULL && node->left == NULL)
+		return (1);
+	else
+		return (0);
+}
