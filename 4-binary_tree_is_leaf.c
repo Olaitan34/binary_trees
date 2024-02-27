a6e9765ded5a1159c1835aefcbef814c4e9ed858
emmfatsneh@gmail.com <emmfatsneh@gmail.com>
2024-02-27 20:03:00 +0100

    la

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,6 +2,9 @@
 /**
  * binary_tree_is_leaf - To check if the node is a leaf
  * @tree: tree to check
+ * Return: 1 if node is a leaf
+ *         0 if not a leaf
+ *         0 if node is NULL
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
